e05c9c24e10f0f166a40a367f866ea5373dc5f67
Liam Dugan <ldugan@seas.upenn.edu>
2017-04-30 12:01:30 -0400

    Implemented Sorting by Readability & Added More Books
    Books now sorted by readability and only your top 8 will show

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -7,30 +7,57 @@ import Book from './Book.jsx';
 
 export default class BookList extends React.Component {
 
+  calcReadability(book) {
+    const vocab = this.props.store.profile.vocab;
+    const text = book.text.split(" ");
+    var knownWords = 0;
+    for (var i = 0; i < text.length; i++) {
+      if (vocab.includes(text[i])) {
+        knownWords++;
+      }
+    }
+    return (knownWords / text.length) * 100;
+  }
+
   // Render the List of books depending on the type of list
   render() {
     const profile = this.props.store.profile;
     const books = this.props.store.books;
 
+    const sortedBooks = books.sort((bookA, bookB) => {
+      if (this.calcReadability(bookA) < this.calcReadability(bookB)) {
+        return 1;
+      } else if (this.calcReadability(bookA) > this.calcReadability(bookB)) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+    var unreadBooksShown = 0;
+    var favBooksShown = 0;
+    var readBooksShown = 0;
     // For all books, render empty span if it doesn't match the criteria
-    const row = books.map((book, c) => {
+    const row = sortedBooks.map((book, c) => {
       // if it's not read, only show in recommendations
       if (!profile.read.includes(book.title)) {
-        if (this.props.type === 'rec') {
-          return ((<Book text={book.text} title={book.title} key={c} store={this.props.store} />));
+        if (this.props.type === 'rec' && unreadBooksShown < 8) {
+            unreadBooksShown++;
+            return ((<Book text={book.text} title={book.title} key={c} store={this.props.store} />));
         } else {
           return (<span key={c}></span>);
         }
       // if it's a favorite, show in both favs and read
       } else if (profile.favs.includes(book.title)) {
-        if (this.props.type === 'fav') {
+        if (this.props.type === 'fav' && favBooksShown < 8) {
+          favBooksShown++;
           return ((<Book text={book.text} title={book.title} key={c} store={this.props.store} />));
         } else {
           return (<span key={c}></span>);
         }
       // if it's read show in read
       } else {
-        if (this.props.type === 'read') {
+        if (this.props.type === 'read' && readBooksShown < 8) {
+          readBooksShown++;
           return ((<Book text={book.text} title={book.title} key={c} store={this.props.store} />));
         } else {
           return (<span key={c}></span>);
